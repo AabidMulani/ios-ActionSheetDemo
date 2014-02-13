5961a45e4611d5b5c580cca33463b4f18c53c7e5
Magneto <Magneto@macmini-109add607741.local>
2014-02-13 13:02:09 +0530

    Initial Commit

---
+++ ActionSheetDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ActionSheetDemo
+//
+//  Created by Magneto on 2/13/14.
+//  Copyright (c) 2014 Magneto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ActionSheetDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ActionSheetDemo
+//
+//  Created by Magneto on 2/13/14.
+//  Copyright (c) 2014 Magneto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
