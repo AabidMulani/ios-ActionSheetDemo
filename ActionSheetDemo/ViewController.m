3fbd5da7d208afe56ba25392baa897683eb9e54f
AabidMulani <mulaniaabid@gmail.com>
2016-02-17 01:05:52 +0530

    Initial Commit

--- ActionSheetDemo/ViewController.m
+++ ActionSheetDemo/ViewController.m
@@ -9,15 +9,44 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UIDatePicker *selectDate;
 
 @end
 
 @implementation ViewController
-
+@synthesize selectDate;
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	NSLog(@"Date Val:%@",[[NSUserDefaults standardUserDefaults] stringForKey:@"date"]);
+}
+- (IBAction)clickMe:(id)sender {
+    
+    NSDateFormatter *format=[[NSDateFormatter alloc]init];
+    [format setDateFormat:@"hh:mm a"];
+    
+    NSString *str=[format stringFromDate:selectDate.date];
+    NSLog(@"Selected Date:%@",str);
+    
+
+    [[NSUserDefaults standardUserDefaults] setObject:str forKey:@"date"];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+    
+    
+    UIActionSheet *action = [[UIActionSheet alloc]initWithTitle:@"Sharing" delegate:self cancelButtonTitle:@"Close" destructiveButtonTitle:nil otherButtonTitles:@"Facebook",@"Twitter",@"Mail", nil];
+    [action showInView:self.view];
+}
+- (void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex
+{
+    if (buttonIndex==0) {
+        NSLog(@"Facebook");
+    }
+    else if (buttonIndex==1){
+        NSLog(@"Twitter");
+    }
+    else if (buttonIndex==2){
+        NSLog(@"MAIL");
+    }
 }
 
 - (void)didReceiveMemoryWarning
